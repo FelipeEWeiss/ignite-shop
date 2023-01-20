88c35d16bc4d1f89a352d593e044decf4cc93965
FelipeWeiss <felipeew19@gmail.com>
2023-01-19 23:40:05 -0300

    improve(product): Styled and structured product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,29 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router'
+
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
 export const Product = () => {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        {/* <Image /> */}
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsa sed distinctio cupiditate at corrupti laborum corporis ipsam omnis alias dignissimos sapiente in recusandae assumenda, fugit laboriosam consectetur vitae excepturi.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
 
 export default Product;
