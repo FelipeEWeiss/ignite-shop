5f153baed92294a00328934ad26bbfc3ef9f5773
FelipeWeiss <felipeew19@gmail.com>
2023-01-19 23:00:23 -0300

    improve(home): Add SSG in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image"
 import { useKeenSlider } from 'keen-slider/react'
 
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 
 import { stripe } from "../lib/stripe"
-
-import shirt1 from '../assets/shirts/1.png'
-import shirt2 from '../assets/shirts/2.png'
-import shirt3 from '../assets/shirts/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 import { HomeContainer, Product } from "../styles/pages/home"
@@ -48,7 +44,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -67,6 +63,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
