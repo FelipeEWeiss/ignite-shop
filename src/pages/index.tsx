a41a53c9728831950be6a9ce203eefdc0bdb7377
FelipeWeiss <felipeew19@gmail.com>
2023-01-19 23:21:10 -0300

    improve(home): Add navigation from Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { useKeenSlider } from 'keen-slider/react'
 
 import { GetStaticProps } from "next"
+import Link from 'next/link'
 
 import { stripe } from "../lib/stripe"
 
@@ -30,14 +31,19 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link
+            key={product.id}
+            href={`/product/${product.id}`}
+          >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
